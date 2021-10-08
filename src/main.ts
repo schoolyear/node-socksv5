c4671051cc2ff310663ddc89184b34af94b569f8
Oliver-Fish <github@oliver.fish>
2021-10-08 14:24:07 +0100

    debug setup

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
-// export * from "./Server";
-// export * from "./Client";
-// export * from "./Auth";
-
 import { Server} from './Server';
 import { Auth } from './Auth';
 
 export function SetupProxy(): Server {
-    // let dnsCache = new Map();
+    let dnsCache = new Map();
     return new Server(
         {
         },
         (info, accept, deny) => {
-			// console.log("request", info)
-            accept().catch((err) => {
-				console.log("accept failed with err", err)
-			});
+            accept();
         },
     );
 }
@@ -24,7 +17,7 @@ export function StartProxy(srv: Server): Promise<void> {
     srv.useAuth(Auth.none());
 
     // wrap the callback in a promise
-    const listen = (port: number, hostname: string) => {
+    const listen = (port, hostname) => {
         return new Promise<void>((resolve, reject) => {
             srv.listen(port, hostname, () => {
                 resolve();
@@ -34,16 +27,3 @@ export function StartProxy(srv: Server): Promise<void> {
 
     return listen(8080, 'localhost');
 }
-
-const svr = SetupProxy()
-
-
-svr.on("error", (err)=> {
-	console.log("emitted error caught", err)
-})
-
-
-svr.emit("error", "look an error!")
-
-
-StartProxy(svr).then(() => { console.log("proxy running")})
